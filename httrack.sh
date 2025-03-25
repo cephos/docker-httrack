b7abae2b531c71dbfa4bdcaae4b7376c51dd5683
cephos <cephos@gmail.com>
2025-03-25 16:45:04 +0100

    override robots

--- httrack.sh
+++ httrack.sh
@@ -14,9 +14,9 @@ echo Mirror external link depth :$EXTERNAL_LINKS_DEPTH
 # Here the magic happens
 if [[ $FULL_CLONE == true ]]; then
     echo "php artisan schedule:run "
-    httrack --update  $FINAL_URL -O /tmp/site -N100 -I0 -N "%p/%n%[month].%t" --max-rate 0 --disable-security-limits --near  -v -c64
+    httrack  --update  $FINAL_URL -O /tmp/site -s0 -N100 -I0 -N "%p/%n%[month].%t" --max-rate 0 --disable-security-limits --near  -v -c64
 else
-    httrack --update $FINAL_URL -O /tmp/httrack -r$MIRROR_DEPTH -%e$EXTERNAL_LINKS_DEPTH -N100 --preserve -%F ''
+    httrack --update $FINAL_URL -O /tmp/httrack -s0 -r$MIRROR_DEPTH -%e$EXTERNAL_LINKS_DEPTH -N100 --preserve -%F ''
     mv /tmp/httrack/index-2.html /tmp/site/index.html
 fi
 
